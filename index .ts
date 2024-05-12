f03532521959ff3342186cb7d66e6413a0cdef03
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-05-12 19:51:32 +0500

    Add files via upload

---
+++ index .ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random()* 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        massage:"please guess a number",
+    },
+]);
+   
+if(answer.userGuessednumber === randomNumber){
+    console.log("congratulation you guessed a right number.")
+}else{
+    console.log("you guessed a wrong number.")
+}
+
+
+
